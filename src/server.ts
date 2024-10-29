24e1842b23574684306b0f2e12cac714e8bc0fb9
PremveerSingh <premcallsmaster@gmail.com>
2024-10-29 17:15:02 +0530

    dsbgv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app";
+import { DB_NAME } from "./constants";
 
 const PORT = process.env.PORT || 3001;
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+})();
 
 app.listen(PORT, () => {
   console.log(`server is succesfully running on port localhost:${PORT}`);
